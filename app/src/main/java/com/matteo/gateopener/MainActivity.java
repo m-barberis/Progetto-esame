6cfb7dcefa78c6728a208c3c657d781001794324
Matteo Barberis <matteo25b@gmail.com>
2025-06-13 11:24:26 +0200

    test della DTW

--- app/src/main/java/com/matteo/gateopener/MainActivity.java
+++ app/src/main/java/com/matteo/gateopener/MainActivity.java
@@ -8,10 +8,16 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.matteo.gateopener.fastdtw.dtw.FastDTW;
+import com.matteo.gateopener.fastdtw.timeseries.TimeSeries;
+import com.matteo.gateopener.fastdtw.util.DistanceFunction;
 import com.matteo.gateopener.interfaces.IRecordingDone;
 import com.matteo.gateopener.misc.Constants;
 import com.matteo.gateopener.mfcc.MFCC_Extractor;
 import com.matteo.gateopener.recorder.Recorder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //import fastdtw.dtw.FastDTW;
 //import fastdtw.timeseries.DoubleTimeSeries;
@@ -26,6 +32,9 @@ public class MainActivity extends AppCompatActivity implements IRecordingDone {
     private Chronometer chronometer;
     private Recorder recorder;
     private MFCC_Extractor mfcc_extractor;
+    private FastDTW fastDTW;
+    private TimeSeries tsTest1, tsTest2;
+    private DistanceFunction distanceFunction;
     private boolean shouldRecordingKeepGoing = false;
     //private File wavFile;
     double[][] mfccMatrix;
@@ -42,6 +51,18 @@ public class MainActivity extends AppCompatActivity implements IRecordingDone {
         initViews();
         recorder = new Recorder(this, Constants.AUDIO_SAMPLING_FREQUENCY, Constants.MAX_RECORDING_TIME_S, Constants.DEFAULT_SILENCE_THRESHOLD, Constants.FRAME_LENGTH_SAMPLES);
         mfcc_extractor = new MFCC_Extractor(Constants.AUDIO_SAMPLING_FREQUENCY, Constants.FRAME_SIZE, Constants.FRAME_HOP_SIZE, Constants.MFCC_COUNT);
+        fastDTW = new FastDTW();
+        distanceFunction = new DistanceFunction() {
+            @Override
+            public double distance(double[] a, double[] b) {
+                double sum = 0;
+                for (int i = 0; i < a.length; i++) {
+                    double d = a[i] - b[i];
+                    sum += d * d;
+                }
+                return Math.sqrt(sum);
+            }
+        };
 
 
         bttRecord.setOnClickListener( (v) -> {
@@ -72,6 +93,24 @@ public class MainActivity extends AppCompatActivity implements IRecordingDone {
         for (int i = 0; i < 400; i++){
             y[i] = (short)i;
         }
+        //Test per DTW
+        List<double[]> xlist1 = new ArrayList<>();
+        double[] x = new double[200];
+        for (int i = 0; i < 200; i++) {
+            x[i] = (double) i;
+        }
+        xlist1.add(x.clone());
+        List<double[]> xlist2 = new ArrayList<>();
+        double[] x2 = new double[200];
+        for (int i = 0; i < 200; i++) {
+            x2[i] = (double) i-1;
+        }
+        xlist2.add(x2.clone());
+        double distance;
+        tsTest1 = new TimeSeries(xlist1);
+        tsTest2 = new TimeSeries(xlist2);
+        distance = FastDTW.getWarpDistance(tsTest1, tsTest2, distanceFunction);
+
         //TODO
         mfccMatrix = mfcc_extractor.extractMFCC(y);
         resetWidgets();
@@ -83,5 +122,9 @@ public class MainActivity extends AppCompatActivity implements IRecordingDone {
         bttRecord.setEnabled(true);
         bttStop.setEnabled(false);
         chronometer.stop();
+    }
+
+    private void testDTW() {
+
     }
 }
